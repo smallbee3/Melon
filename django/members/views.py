f0d867698e9bcea8b446f5a3643a6792986853ae
smallbee <smallbee3@gmail.com>
2018-02-23 16:52:30 +0900

    SignupForm에서 각 field에 특정한 validation함수 구현

--- django/members/views.py
+++ django/members/views.py
@@ -56,21 +56,24 @@ def signup_view(request):
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            password2 = form.cleaned_data['password2']
+            # password2 = form.cleaned_data['password2']
+            User.objects.create_user(username=username, password=password)
+            return redirect('index')
 
-            is_valid = True
-            if User.objects.filter(username=username).exists():
-                form.add_error('username', '이미 사용되고 있는 아이디입니다.')
-                is_valid = False
 
-            if password != password2:
-                form.add_error('password2', '비밀번호와 비밀번호 확인란의 값이 다릅니다.')
-                is_valid = False
-
-            if is_valid:
-                # create_superuser의 경우에는 email이 필수?
-                User.objects.create_user(username=username, password=password)
-                return redirect('index')
+            # is_valid = True
+            # if User.objects.filter(username=username).exists():
+            #     form.add_error('username', '이미 사용되고 있는 아이디입니다.')
+            #     is_valid = False
+            #
+            # if password != password2:
+            #     form.add_error('password2', '비밀번호와 비밀번호 확인란의 값이 다릅니다.')
+            #     is_valid = False
+            #
+            # if is_valid:
+            #     # create_superuser의 경우에는 email이 필수?
+            #     User.objects.create_user(username=username, password=password)
+            #     return redirect('index')
 
     else:
         form = SignupForm()
